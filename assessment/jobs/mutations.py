f28609e5c7d5b09f134ee49d6ddd19b7c0ff58b5
Kevin Dai <kevin.dai.2451@gmail.com>
2020-09-08 15:04:44 -0400

    changes to mutations

--- assessment/jobs/mutations.py
+++ assessment/jobs/mutations.py
@@ -1,22 +1,98 @@
 import graphene
 from graphene_django import DjangoObjectType
 from assessment.jobs.models import Example as ExampleModel
+from assessment.jobs.models import Job 
+from assessment.jobs.models import Category
+from assessment.jobs.models import JobCategory
 
 
-class ExampleType(DjangoObjectType):
+class CategoryType(DjangoObjectType):
     class Meta:
-        model = ExampleModel
+        model = Category
 
+class CreateCategory(graphene.Mutation):
+    id = graphene.Int()
+    name = graphene.String()
 
-class Example(graphene.Mutation):
     class Arguments:
-        example_arg = graphene.String()
+        name = graphene.String()
+    
+    def mutate(self, info, name):
+        category = Category(name=name)
+        category.save()
 
-    example_arg = graphene.Field(ExampleType)
+        return CreateCategory(
+            id=category.id,
+            name=category.name,
+        )
 
-    def mutate(self, example_arg):
-        pass
 
+
+
+class JobType(DjangoObjectType):
+    class Meta:
+        model = Job
+
+class CreateJob(graphene.Mutation):
+    id = graphene.Int()
+    title = graphene.String()
+
+    class Arguments:
+        title = graphene.String()
+
+    def mutate(self, info, title):
+        job = Job(title=title)
+        job.save()
+
+        return CreateJob(
+            id=job.id,
+            title=job.title,
+        )
+
+
+
+
+class JobCategoryType(DjangoObjectType):
+    class Meta:
+        model = JobCategory
+
+class CreateJobCategory(graphene.Mutation):
+    id = graphene.Int()
+    jobID = graphene.Int()
+    categoryID = graphene.Int()
+
+    class Arguments:
+        jobID = graphene.Int()
+        categoryID = graphene.Int()
+
+    def mutate(self, info, jobID, categoryID):
+        jobcategory = JobCategory(jobID=jobID, categoryID=categoryID)
+        jobcategory.save()
+
+        return CreateJobCategory(
+            id=jobcategory.id,
+            jobID=jobcategory.jobID,
+            categoryID=jobcategory.categoryID,
+        )
+
+
+
+
+
+
+class Query(graphene.ObjectType):
+    categories = graphene.List(CategoryType)
+    jobs = graphene.List(JobType)
+    jobcategories = graphene.List(JobCategoryType)
+
+    def resolve_categories(self, info):
+        return Category.objects.all()
+    
+    def resolve_jobs(self, info):
+        return Job.objects.all()
+    
+    def resolve_jobcategories(self, info):
+        return JobCategory.objects.all()
 
 class Mutation(graphene.ObjectType):
-    example_arg = Example.Field()
+    create_jobcategory = CreateJobCategory.Field()
--- assessment/jobs/schema.py
+++ assessment/jobs/schema.py
@@ -1,5 +1,49 @@
 import graphene
+from graphene_django import DjangoObjectType
+from assessment.jobs.models import Job
+from assessment.jobs.models import Category
+from assessment.jobs.models import JobCategory
+
+class JobType(DjangoObjectType):
+    class Meta:
+        model = Job
+        fields = ("id", "title")
+
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+        fields = ("id", "name")
+
+class JobCategoryType(DjangoObjectType):
+    class Meta:
+        model = JobCategory
+        fields = ("id", "jobID", "categoryID")
 
 
 class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hi Test!")
+    jobs = graphene.List(JobType)
+    job_by_id = graphene.Field(JobType, id = graphene.String())
+
+    categories = graphene.List(CategoryType)
+    category_by_id = graphene.Field(CategoryType, id = graphene.String())
+
+    jobcategories = graphene.List(JobCategoryType)
+    jobcategory_by_id = graphene.Field(JobCategoryType, id = graphene.String())
+
+    def resolve_jobs(self, info, **kwargs):
+        return Job.objects.all()
+
+    def resolve_question_by_id(self, info, id):
+        return Job.objects.get(pk=id)
+
+    def resolve_categories(self, info, **kwargs):
+        return Category.objects.all()
+
+    def resolve_category_by_id(self, info, id):
+        return Category.objects.get(pk=id)
+
+    def resolve_jobcategories(self, info, **kwargs):
+        return JobCategory.objects.all()
+
+    def resolve_jobcategory_by_id(self, info, **kwargs):
+        return JobCategory.objects.get(pk=id)
